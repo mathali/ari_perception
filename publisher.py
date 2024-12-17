80449b33654e8103004ceca280ee6fb2e9c86e91
mathali <matej.halinkovic@gmail.com>
2024-12-17 14:46:08 +0100

    added image publishers

--- publisher.py
+++ publisher.py
@@ -2,6 +2,9 @@
 
 import rospy
 from std_msgs.msg import String, Bool
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import numpy as np
 
 
 class PedestrianPublisher():
@@ -9,7 +12,7 @@ class PedestrianPublisher():
     def __init__(self):
         self.publisher = rospy.Publisher('pedestrian_publisher', String, queue_size=20)
         rospy.init_node('pedestrian_publisher', anonymous=True)
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(20)
 
 
     def publish(self, message):
@@ -18,13 +21,52 @@ class PedestrianPublisher():
             self.publisher.publish(message)
             stop = True
 
+class FrontImagePublisher:
+    def __init__(self):
+        self.publisher = rospy.Publisher('front_camera_image', Image, queue_size=20)
+        self.br = CvBridge()
+        self.rate = rospy.Rate(20)
+
+    def publish(self, image: np.ndarray) -> None:
+        """
+        Publish front camera image
+        
+        Args:
+            image: numpy array containing image data
+        """
+        try:
+            img_msg = self.br.cv2_to_imgmsg(image)
+            self.publisher.publish(img_msg)
+        except Exception as e:
+            rospy.logerr(f"Error publishing front camera image: {e}")
+
+class BackImagePublisher:
+    def __init__(self):
+        self.publisher = rospy.Publisher('back_camera_image', Image, queue_size=20)
+        self.br = CvBridge()
+        self.rate = rospy.Rate(20)
+
+    def publish(self, image: np.ndarray) -> None:
+        """
+        Publish back camera image
+        
+        Args:
+            image: numpy array containing image data
+        """
+        try:
+            img_msg = self.br.cv2_to_imgmsg(image)
+            self.publisher.publish(img_msg)
+        except Exception as e:
+            rospy.logerr(f"Error publishing back camera image: {e}")
+
+
 
 class PedestrianLeftPublisher():
 
     def __init__(self):
         self.publisher = rospy.Publisher('topic_p_left', Bool, queue_size=20)
         # rospy.init_node('topic_p_left', anonymous=True)
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(20)
 
     def publish(self, flag: bool) -> None:
         """Publish a boolean flag to the topic"""
@@ -36,7 +78,7 @@ class PedestrianRightPublisher():
     def __init__(self):
         self.publisher = rospy.Publisher('topic_p_right', Bool, queue_size=20)
         # rospy.init_node('topic_p_right', anonymous=True)
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(20)
 
     def publish(self, flag: bool) -> None:
         """Publish a boolean flag to the topic"""
@@ -48,7 +90,7 @@ class VehicleLeftPublisher():
     def __init__(self):
         self.publisher = rospy.Publisher('topic_t_left', Bool, queue_size=20)
         # rospy.init_node('topic_t_left', anonymous=True)
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(20)
 
     def publish(self, flag: bool) -> None:
         """Publish a boolean flag to the topic"""
@@ -60,7 +102,7 @@ class VehicleRightPublisher():
     def __init__(self):
         self.publisher = rospy.Publisher('topic_t_right', Bool, queue_size=20)
         # rospy.init_node('topic_t_right', anonymous=True)
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(20)
 
     def publish(self, flag: bool) -> None:
         """Publish a boolean flag to the topic"""
